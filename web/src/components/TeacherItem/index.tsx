0ca4d48ce99ef4a9b66b8174f0373805fe73db34
Alex <alex.silva.11@hotmail.com>
2020-08-06 14:42:40 -0300

    Aula 4

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,42 +1,56 @@
 import React from "react";
+import api from "../../services/api";
 
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
-import './styles.css'
+import "./styles.css";
 
-function TeacherItem() {
+export interface Teacher {
+    avatar: string;
+    bio: string;
+    cost: number;
+    id: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps>=({teacher}) => {
+  function createNewConneciton(){
+    api.post('connections',{
+      user_id: teacher.id
+    })
+  }
   return (
     <article className="teacher-item">
       <header>
         <img
-          src="https://avatars2.githubusercontent.com/u/60552754?s=460&u=cd39e1b1bdb94e1fa08acd866e2955867911fc00&v=4"
-          alt="Alex Silva"
+          src={teacher.avatar}
+          alt={teacher.name}
         />
         <div>
-          <strong>Alex Silva</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
       <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br />
-        <br />
-        Apaixonado por explodir coisas em laboratório e por mudar a vida das
-        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
-        uma das minhas explosões.
+        {teacher.bio}
       </p>
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>{teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a target="_blank" onClick={createNewConneciton} href={`https://wa.me/${teacher.whatsapp}`} type="button">
           <img src={whatsappIcon} alt="WhatsApp" />
           Entrar em Contato
-        </button>
+        </a>
       </footer>
     </article>
   );
 }
 
-export default TeacherItem
+export default TeacherItem;
