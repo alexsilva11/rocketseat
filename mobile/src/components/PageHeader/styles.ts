37b4154290594d65359910a4f70c568b7c424e3d
Alex <alex.silva.11@hotmail.com>
2020-08-07 15:10:12 -0300

    5 aula

--- mobile/src/components/PageHeader/styles.ts
+++ mobile/src/components/PageHeader/styles.ts
@@ -6,18 +6,23 @@ const styles = StyleSheet.create({
     backgroundColor: "#8257e5",
   },
   topBar: {
-      flexDirection: "row",
-      alignItems: "center",
-      justifyContent: "space-between",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
   },
   title: {
-      fontFamily: 'Archivo_700Bold',
-      color: '#fff',
-      fontSize: 24,
-      lineHeight: 32,
-      maxWidth: 160,
-      marginVertical: 40,
-  }
+    fontFamily: "Archivo_700Bold",
+    color: "#fff",
+    fontSize: 24,
+    lineHeight: 32,
+    maxWidth: 160,
+    marginVertical: 40,
+  },
 });
 
 export default styles;
